52b4534473b4074510a4e1eff768d0c7004adb32
shivam tyagi <shivamt.ug19.ec@nitp.ac.in>
2022-05-27 11:12:18 +0530

    1

---
+++ day4/Midlist.java
@@ -0,0 +1,32 @@
+package day4;
+
+import parent_class.*;
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+        }
+        return slow;
+
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        head.next.next.next.next.next.next = new ListNode(7);
+        Solution s = new Solution();
+        ListNode res = s.middleNode(head);
+        System.out.println(res.val);
+
+    }
+}
